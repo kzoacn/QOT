5f32d107be3ec4b3722ef2516960f3454b6bae30
kzoacn <kzoacn@outlook.com>
2020-09-16 22:08:52 +0800

    analyz

--- main.cpp
+++ main.cpp
@@ -176,8 +176,118 @@ void QOT(int party,vector<Qubit>&alice,vector<Qubit>&bob,string message[2],bool 
     ret=message[b];
 }
 
+char to_hex_ch(unsigned char hex_val)
+{
+  if (hex_val >= 0 && hex_val <= 9)
+    return char(hex_val + '0');
+  else
+    return char(hex_val - 10) + 'A';
+}
+
+std::string dec_to_hex(unsigned int dec)
+{
+  std::string hex = "";
+  if (dec == 0)
+    hex = "0";
+
+  while (dec != 0)
+  {
+    unsigned int hex_val = dec % 16;
+    hex = to_hex_ch(hex_val) + hex;
+    dec /= 16;
+  }
+  return hex;
+}
+
+bool out=false;
+int all=0;
+
+
+vector<Qubit> read_frame(ifstream &fin){
+    int cnt=0;
+    unsigned char tmp[4096];
+    fin.read((char*)tmp,8);
+   
+    if(tmp[0]==0xA0 && tmp[1]==0xA1 && tmp[2]==0xA2 && tmp[3]==0xA3){
+        
+    }else{
+        
+    }
+ 
+    int length=(1*tmp[4]<<8) | tmp[5];
+    length=(length-3)/2;
+    fin.read((char*)tmp,4);
+    all++;
+    if(length>100){
+        exit(0);
+    }
+    int h=0;
+    vector<Qubit>qubits;
+    for(int i=0;i<length;i++){
+        fin.read((char*)tmp,4);
+        h=h*233+tmp[1];
+        //if(out)
+        ///    cout<<cnt<<" "<<dec_to_hex(tmp[0])<<" "<<dec_to_hex(tmp[1])<<" "<<dec_to_hex(tmp[2])<<" "<<dec_to_hex(tmp[3])<<endl;
+        Qubit qb;
+        qb.basis=tmp[3]/2%2;
+        qb.value=tmp[3]%2;
+        qubits.push_back(qb);
+    }
+    fin.read((char*)tmp,6);
+    return qubits;
+}
+void parse(string file){
+    ifstream fin(file,ios::in|ios::binary);
+    
+}
 
 int main(){
+
+
+    
+    ifstream ain("./rawkey/alice/key.bin",ios::in|ios::binary);
+    ifstream bin("./rawkey/bob/key.bin",ios::in|ios::binary);
+
+
+    int cnt=0;
+
+    int cnt_same_basis=0;
+    int cnt_diff_basis=0;
+
+    int cnt_same_same=0;
+    int cnt_diff_same=0;
+
+    for(int j=1;j<=100000;j++){
+        cout<<j<<endl;
+        puts("A---------");
+        auto vec1=read_frame(ain);//1318325
+        puts("B---------"); 
+        auto vec2=read_frame(bin);
+
+        assert(vec1.size()==vec2.size());
+        for(int i=0;i<vec1.size();i++){
+            Qubit qb1=vec1[i];
+            Qubit qb2=vec2[i];
+            if(qb1.basis==qb2.basis){
+                cnt_same_basis++;
+                if(qb1.value==qb2.value)
+                    cnt_same_same++;
+            }else{
+                cnt_diff_basis++;
+                if(qb1.value==qb2.value)
+                    cnt_diff_same++;    
+            }
+        }
+        printf("cnt same basis:%d\n",cnt_same_basis);
+        printf("cnt diff basis:%d\n",cnt_diff_basis);
+
+        printf("cnt same basis same value:%d/%d\n",cnt_same_same,cnt_same_basis);
+        printf("cnt diff basis same value:%d/%d\n",cnt_diff_same,cnt_diff_basis);
+
+    }
+    return 0;
+
+
     srand(time(0));
     string m[2];
     m[0]="1111111111111";
